ca502729f7b91fa415dcd8da10fe6766d24fa3ed
EbenZhang <zhangyingneng@gmail.com>
2017-09-20 22:33:42 +0800

    Add unit test

--- SqlServerUtils.Metadata.Tests/Properties/AssemblyInfo.cs
+++ SqlServerUtils.Metadata.Tests/Properties/AssemblyInfo.cs
@@ -2,36 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Nicologies.SqlServerUtils.Metadata")]
-[assembly: AssemblyDescription("SqlServer utilities to provide information like instances and databases")]
+[assembly: AssemblyTitle("SqlServerUtils.Metadata.Test")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nicologies")]
-[assembly: AssemblyProduct("Nicologies.SqlServerUtils.Metadata")]
+[assembly: AssemblyProduct("SqlServerUtils.Metadata.Test")]
 [assembly: AssemblyCopyright("Copyright © Nicologies 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("SqlServerUtils.Metadata.Test")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("05015b51-667c-476b-8c15-00d49b6c8508")]
+[assembly: Guid("1583265a-1d13-43f1-aada-21007a6f67a1")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ SqlServerUtils.Metadata.Tests/TestInstancePathConversion.cs
@@ -0,0 +1,33 @@
+﻿using Nicologies.SqlServerUtils.Metadata;
+using NUnit.Framework;
+
+namespace SqlServerUtils.Metadata.Test
+{
+    [TestFixture]
+    public class TestInstancePathConversion
+    {
+        [Test]
+        public void GivenDefaultInstancePath_ShouldReturnDot()
+        {
+            var path = InstancePathConversion.GetInstsPath("LOCALHOST", "MSSQLSERVER");
+            Assert.That(path, Is.EqualTo("."));
+
+            path = InstancePathConversion.GetInstsPath(".", "MSSQLSERVER");
+            Assert.That(path, Is.EqualTo("."));
+        }
+
+        [Test]
+        public void LocalHostShouldBeReplacedWithDot()
+        {
+            var path = InstancePathConversion.GetInstsPath("LOCALHOST", "SQLEXPRESS");
+            Assert.That(path, Is.EqualTo(@".\SQLEXPRESS"));
+        }
+
+        [Test]
+        public void GiveAnInstNameContainsMSSQLSERVER_ShouldntBeReplaced()
+        {
+            var path = InstancePathConversion.GetInstsPath("LOCALHOST", "MSSQLSERVER2012");
+            Assert.That(path, Is.EqualTo(@".\MSSQLSERVER2012"));
+        }
+    }
+}
